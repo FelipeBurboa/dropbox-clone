f498b72ca4c61e539f0ba35a432d2bad2df836e0
Felipe Burboa <felipeburboa1991@gmail.com>
2024-01-01 19:58:29 -0300

    adding clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="">
       <h1>Dropbox Clone</h1>
+      <UserButton />
     </main>
   );
 }
