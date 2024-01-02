1c4fadb40903773c69689ea3b0572571a7bdd5f4
Felipe Burboa <felipeburboa1991@gmail.com>
2024-01-01 21:03:46 -0300

    adding dropzone

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs";
 
 function Dashboard() {
   const { userId } = auth();
-  return <div>Dashboard</div>;
+  return <div>Dashboardd</div>;
 }
 
 export default Dashboard;
